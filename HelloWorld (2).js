7ca806c1b30b8c4c5ee9f690a58f4cb6848cbbfa
Yaswanth kumar <93417766+Yaswakumar@users.noreply.github.com>
2022-01-28 13:30:09 +0530

    Add files via upload

---
+++ HelloWorld (2).js
@@ -0,0 +1,9 @@
+const baseValue = ('Enter the base of a triangle: ');
+const heightValue = ('Enter the height of a triangle: ');
+
+// calculate the area
+const areaValue = (5 * 6) / 2;
+
+console.log(
+  `The area of the triangle is ${areaValue}`
+);
